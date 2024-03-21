fcf4a3439a1aeabed5bb92e4097ecaa9c22b36ad
Naokogit <leandro001sotgia@gmail.com>
2024-03-21 11:13:06 +0100

    Fix suggest.js

--- commands/suggestion/suggest.js
+++ commands/suggestion/suggest.js
@@ -29,140 +29,135 @@ module.exports = {
      * @param {ChatInputCommandInteraction} interaction
      */
     async execute(interaction) {
+        const modal = new ModalBuilder()
+            .setTitle("CREA UN FEEDBACK")
+            .setCustomId(`suggestion-${interaction.user.id}`);
+
+        const titleInput = new TextInputBuilder()
+            .setCustomId("suggestion-title")
+            .setLabel("Titolo del feedback")
+            .setStyle(TextInputStyle.Short)
+            .setPlaceholder("Scrivi il titolo")
+            .setRequired(true);
+        const suggestionInput = new TextInputBuilder()
+            .setCustomId("suggestion-input")
+            .setLabel("Descrivi la proposta")
+            .setStyle(TextInputStyle.Paragraph)
+            .setPlaceholder("Descrivi la proposta")
+            .setRequired(true);
+
+        if(debug){
+            titleInput.setValue("Lorem Ipsum");
+            suggestionInput.setValue("Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam eaque ipsa, quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt, explicabo. Nemo enim ipsam voluptatem, quia voluptas sit, aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos, qui ratione voluptatem sequi nesciunt");
+        }
+
+        modal.addComponents(
+            new ActionRowBuilder().addComponents(titleInput),
+            new ActionRowBuilder().addComponents(suggestionInput)
+        );
+
+        await interaction.showModal(modal);
+
+        const modalInteraction = await interaction
+            .awaitModalSubmit({
+                time: 1000 * 60 * 30
+            })
+            .catch((err) => console.log(err));
+
+        await modalInteraction.deferReply({ ephemeral: true });
+
+        let suggestionMessage;
+
+        const suggestion_channel = interaction.client.channels.cache.get(suggestionsChannelId);
+
+        
+        const suggestionText = modalInteraction.fields.getTextInputValue("suggestion-input");
+        const suggestionTitle = modalInteraction.fields.getTextInputValue("suggestion-title");
+        
         try {
-            const modal = new ModalBuilder()
-                .setTitle("CREA UN FEEDBACK")
-                .setCustomId(`suggestion-${interaction.user.id}`);
-
-            const titleInput = new TextInputBuilder()
-                .setCustomId("suggestion-title")
-                .setLabel("Titolo del feedback")
-                .setStyle(TextInputStyle.Short)
-                .setPlaceholder("Scrivi il titolo")
-                .setMinLength(5)
-                .setMaxLength(100)
-                .setRequired(true);
-            const suggestionInput = new TextInputBuilder()
-                .setCustomId("suggestion-input")
-                .setLabel("Descrivi la proposta")
-                .setStyle(TextInputStyle.Paragraph)
-                .setPlaceholder("Descrivi la proposta")
-                .setMinLength(50)
-                .setMaxLength(4000)
-                .setRequired(true);
-
-            if(debug){
-                titleInput.setValue("Lorem Ipsum");
-                suggestionInput.setValue("Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam eaque ipsa, quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt, explicabo. Nemo enim ipsam voluptatem, quia voluptas sit, aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos, qui ratione voluptatem sequi nesciunt");
-            }
-
-            modal.addComponents(
-                new ActionRowBuilder().addComponents(titleInput),
-                new ActionRowBuilder().addComponents(suggestionInput)
+            suggestionMessage = await suggestion_channel.send(
+                "Creating suggestion, wait..."
             );
+        } catch (error) {
+            modalInteraction.editReply("Failed to create suggestion message");
+            return;
+        }
 
-            await interaction.showModal(modal);
+        const newSuggestion = suggestion({
+            autoIncrement: 0,
+            userId: interaction.user.id,
+            messageId: suggestionMessage.id,
+            title: suggestionTitle,
+            content: suggestionText,
+        });
 
-            const modalInteraction = await interaction
-                .awaitModalSubmit({
-                    time: 1000 * 60 * 30
-                })
-                .catch((err) => console.log(err));
+        await newSuggestion.save();
 
-            await modalInteraction.deferReply({ ephemeral: true });
-
-            let suggestionMessage;
-
-            const suggestion_channel = interaction.client.channels.cache.get(suggestionsChannelId);
-
-            try {
-                suggestionMessage = await suggestion_channel.send(
-                    "Creating suggestion, wait..."
-                );
-            } catch (error) {
-                modalInteraction.editReply("Failed to create suggestion message");
-                return;
-            }
-
-            const suggestionText = modalInteraction.fields.getTextInputValue("suggestion-input");
-            const suggestionTitle = modalInteraction.fields.getTextInputValue("suggestion-title");
-
-            const newSuggestion = suggestion({
-                autoIncrement: 0,
-                userId: interaction.user.id,
-                messageId: suggestionMessage.id,
-                title: suggestionTitle,
-                content: suggestionText,
-            });
-
-            await newSuggestion.save();
-
-            modalInteraction.editReply('Proposta creata!');
+        modalInteraction.editReply('Proposta creata!');
+        
+        const embed = new EmbedBuilder()
+            .setAuthor({
+                name: interaction.user.username,
+                iconURL: interaction.user.displayAvatarURL({ size: 256 })
+            })
+            .addFields([
+                {name: '• TITOLO', value: suggestionTitle},
+                {name: '• DESCRIZIONE', value: suggestionText},
+                {name: '• STATO', value: "⏳ In attesa"},
+                {name: '• VOTI', value: formatResults()}
+            ])
+            .setColor('Orange');
             
-            const embed = new EmbedBuilder()
-                .setAuthor({
-                    name: interaction.user.username,
-                    iconURL: interaction.user.displayAvatarURL({ size: 256 })
-                })
-                .addFields([
-                    {name: '• TITOLO', value: suggestionTitle},
-                    {name: '• DESCRIZIONE', value: suggestionText},
-                    {name: '• STATO', value: "⏳ In attesa"},
-                    {name: '• VOTI', value: formatResults()}
-                ])
-                .setColor('Orange');
-                
-            const upvoteButton = new ButtonBuilder()
-                .setEmoji(emojis.upvote)
-                .setLabel('Upvote')
-                .setStyle(ButtonStyle.Primary)
-                .setCustomId(`suggestion.${newSuggestion.autoIncrement}.upvote`)
+        const upvoteButton = new ButtonBuilder()
+            .setEmoji(emojis.upvote)
+            .setLabel('Upvote')
+            .setStyle(ButtonStyle.Primary)
+            .setCustomId(`suggestion.${newSuggestion.autoIncrement}.upvote`)
+        
+        const downvoteButton = new ButtonBuilder()
+            .setEmoji(emojis.downvote)
+            .setLabel('Downvote')
+            .setStyle(ButtonStyle.Primary)
+            .setCustomId(`suggestion.${newSuggestion.autoIncrement}.downvote`)
             
-            const downvoteButton = new ButtonBuilder()
-                .setEmoji(emojis.downvote)
-                .setLabel('Downvote')
-                .setStyle(ButtonStyle.Primary)
-                .setCustomId(`suggestion.${newSuggestion.autoIncrement}.downvote`)
-                
-            const approveButton = new ButtonBuilder()
-                .setEmoji('💯')
-                .setLabel('Approva')
-                .setStyle(ButtonStyle.Success)
-                .setCustomId(`suggestion.${newSuggestion.autoIncrement}.approve`)
-                
-            const rejectButton = new ButtonBuilder()
-                .setEmoji('🗑')
-                .setLabel('Rifiuta')
-                .setStyle(ButtonStyle.Danger)
-                .setCustomId(`suggestion.${newSuggestion.autoIncrement}.reject`)
-                
-                
-            const firstRow = new ActionRowBuilder().addComponents(upvoteButton, downvoteButton);
-            const secondRow = new ActionRowBuilder().addComponents(approveButton, rejectButton);
-                
-            await suggestionMessage.edit({
-                content: `${interaction.user} ha fatto una proposta!`,
-                embeds: [embed],
-                components: [firstRow, secondRow]
-            });
+        const approveButton = new ButtonBuilder()
+            .setEmoji('💯')
+            .setLabel('Approva')
+            .setStyle(ButtonStyle.Success)
+            .setCustomId(`suggestion.${newSuggestion.autoIncrement}.approve`)
+            
+        const rejectButton = new ButtonBuilder()
+            .setEmoji('🗑')
+            .setLabel('Rifiuta')
+            .setStyle(ButtonStyle.Danger)
+            .setCustomId(`suggestion.${newSuggestion.autoIncrement}.reject`)
             
             
-            const targetMessage = await suggestion_channel.messages.fetch(suggestionMessage.id);
+        const firstRow = new ActionRowBuilder().addComponents(upvoteButton, downvoteButton);
+        const secondRow = new ActionRowBuilder().addComponents(approveButton, rejectButton);
             
-            const thread = await targetMessage.startThread({
-                name: `${suggestionTitle}`,
-                autoArchiveDuration: 60,
-                reason: `${suggestionInput}`
-            });
+        await suggestionMessage.edit({
+            content: `${interaction.user} ha fatto una proposta!`,
+            embeds: [embed],
+            components: [firstRow, secondRow]
+        });
+        
+        
+        const targetMessage = await suggestion_channel.messages.fetch(suggestionMessage.id);
+        
+        const thread = await targetMessage.startThread({
+            name: `${suggestionTitle}`,
+            autoArchiveDuration: 60,
+            reason: `${suggestionInput}`
+        });
 
-            // console.log(thread);
-            // newSuggestion.threadId = thread.id;
-            const data = await suggestion.updateOne({autoIncrement: newSuggestion.autoIncrement}, { $set: { threadId: thread.id } });
-            console.log(newSuggestion.autoIncrement);
-            console.log(data);
+        // console.log(thread);
+        // newSuggestion.threadId = thread.id;
+        const data = await suggestion.updateOne({autoIncrement: newSuggestion.autoIncrement}, { $set: { threadId: thread.id } });
+        console.log(newSuggestion.autoIncrement);
+        console.log(data);
 
-            // await newSuggestion.save();
-            
-        } catch(err) { console.log(err)}
+        // await newSuggestion.save();
+        
     },
 };
